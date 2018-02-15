5fafaf43b229b6617e6affe43b574dd2cd879883
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-02-15 16:27:41 +0700

    add example loop

--- loop/main.go
+++ loop/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var fruits = []string{"Banana", "Melon", "Potato", "Coconut"}
+
+	for index := 0; index < len(fruits); index++ {
+		fmt.Printf("index: %d, value: %s\n", index, fruits[index])
+	}
+
+	for index, value := range fruits {
+		fmt.Printf("index: %d, value: %s\n", index, value)
+	}
+
+	var index int
+	for {
+		if index == len(fruits) {
+			break
+		}
+		fmt.Printf("index: %d, value: %s\n", index, fruits[index])
+		index++
+	}
 }
