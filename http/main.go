74b1c35aae4b7ad6e79f815908981d2c1b0cb4af
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-02-15 16:31:42 +0700

    add example http server

--- http/main.go
+++ http/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
+)
+
+var httpPort = os.Getenv("PORT")
+
+func main() {
+	http.HandleFunc("/", homeHandle)
+	http.ListenAndServe(":"+httpPort, handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
+}
+
+func homeHandle(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
